e3ca0071ef0d846d09cbca73341cba358dbe3766
PijusRan <pijusranonis@gmail.com>
2026-01-21 13:56:28 +0200

    Rating history + started with history

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
+import { Rating } from '@mui/material'
 import './App.css'
 
 import list from "./assets/list.json"
@@ -7,31 +8,55 @@ import missingArt from "./assets/missingArt.png"
 
 
 function App() {
-    const [albumTitle, loadTitle] = useState("Press \"Generate\" to begin.")
+	const [EntryHistory, setEntryHistory] = useState(localStorage.getItem('entries'));
+    const [AlbumData, loadAlbumData] = useState([null,null, null , null])
+	const [headerString, loadHeader] = useState("Press \"Generate\" to begin.")
 	const [albumImg, loadCoverImg] = useState(placeholderImg)
 	const [buttonDivCSS, setButtonDivCSS] = useState("buttonDiv");
+	const [rateButtonCSS, setRateButtonCSS] = useState("rateButton disable");
 	const [rateDivCSS, setRateDivCSS] = useState("rateDiv disable");
-
+	const [ratingValue, setRatingVal] = useState(0);
+	
 	async function generateAlbum(){
 		//Pick random album
 		const randomIndex = Math.floor(Math.random() * list.length);
-		const album = list[randomIndex].match(/^(.*?)\s*-\s*(.*?)\s*\((.*?)\)$/);
-		loadTitle(album[1] + " - " + album[2]);
+		loadHeader(list[randomIndex]);
 
+		let album:Array<string> | null;
+		album = list[randomIndex].match(/^(.*?)\s*-\s*(.*?)\s*\((.*?)\)$/);
+		loadAlbumData(album);
+		
 		//Find cover
-		const deezerResponse = await fetch(`https://www.theaudiodb.com/api/v1/json/123/searchalbum.php?s=${album[1]}&a=${album[2]}`);
-		console.log(deezerResponse);
-		const deezerJSON = await deezerResponse.json();
+		const dbResponse = await fetch(`https://www.theaudiodb.com/api/v1/json/123/searchalbum.php?s=${album[1]}&a=${album[2]}`);
+		console.log(dbResponse);
+		const dbJSON = await dbResponse.json();
 		
 		//Load cover
-		if(deezerJSON.album) loadCoverImg(deezerJSON.album[0].strAlbumThumb)
+		if(dbJSON.album) loadCoverImg(dbJSON.album[0].strAlbumThumb)
 		else loadCoverImg(missingArt);
+
+		//Enable rating
+		setRateButtonCSS("rateButton");
 	}
+
 	function rateFunction(){
 		setButtonDivCSS("buttonDiv disable");
 		setRateDivCSS("rateDiv");
 	}
+	function changedStars(event: React.SyntheticEvent, value: number | null){
+		setRatingVal(value);
+	}
 	function doneRating(){
+		// Creates entry
+		const albumEntry = {
+			"albumString": AlbumData[0],
+			"albumImage": albumImg,
+			"rating": ratingValue,
+			"date": new Date().toISOString().split('T')[0]
+		}
+
+		// Check if list is empty
+
 		setButtonDivCSS("buttonDiv");
 		setRateDivCSS("rateDiv disable");
 	}
@@ -39,13 +64,14 @@ function App() {
     return (
       <>
 	  	<section className='albumSection'>
-			<h2 className='albumHeader'>{albumTitle}</h2>
+			<h2 className='albumHeader'>{headerString}</h2>
 			<img src={albumImg} className="albumImg"/>
 			<div className={buttonDivCSS}>
 				<button onClick={generateAlbum} className='genButton'>GENERATE</button>
-				<button onClick={rateFunction} className='rateButton'>RATE</button>
+				<button onClick={rateFunction} className={rateButtonCSS}>RATE</button>
 			</div>
 			<div className={rateDivCSS}>
+				<Rating size='large' onChange={changedStars} value={ratingValue} className='ratingStars'/>
 				<button onClick={doneRating}>SUBMIT</button>
 			</div>
 
