af3450d8b1fd43b62ffb08e2774176249fd69887
PijusRan <pijusranonis@gmail.com>
2026-02-06 17:53:47 +0200

    Added types in history

--- src/components/histWindow.tsx
+++ src/components/histWindow.tsx
@@ -1,12 +1,18 @@
 import { Rating } from '@mui/material'
 import "./histWindow.css"
 
+interface EntryType {
+    albumImage: string;
+    albumString: string;
+    rating: number;
+}
+
 export default function histWindow(props : any){
     return(
         <section className='histSection'>
             <h2>Listening History</h2>
             <figure className='histFigure'>
-                {(props.entryHistory) ? (props.entryHistory).map((entry, id) => {return(
+                {(props.entryHistory) ? (props.entryHistory).map((entry : EntryType, id : number) => {return(
                     <figcaption className="entryItem" key={id}>
                         <img src={entry.albumImage}/>
                         <p>
