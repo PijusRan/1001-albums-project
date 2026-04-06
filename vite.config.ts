a5a12b96d8391f4349473b1b54af8795a3bd5168
PijusRan <pijusranonis@gmail.com>
2026-04-06 13:27:47 +0300

    Better mobile support

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/1001-albums-project/',
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: false
+  }
 })
